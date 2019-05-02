e64bb44189537d8a964a966f17b09ec36cde1d91
diegovillafuerte <diegovillafuertesoraiz@gmail.com>
2019-05-02 15:47:05 -0500

    rollback

--- rentizapan/pages/index.vue
+++ rentizapan/pages/index.vue
@@ -1,12 +1,49 @@
 <template>
-  <v-layout row wrap justify-center align-center>
-    <v-card v-for="(item, i) in casas" :key="i" @click="$router.push(`/property/${item.id}`)">
-      <v-flex xs6>
-        <div>
-          {{ item.titulo }} - {{ item.img_url }}
-        </div>
-      </v-flex>
-    </v-card>
+  <v-layout justify-center>
+    <v-flex xs12 sm6>
+      <v-card v-for="(item, i) in casas" :key="i" @click="$router.push(`/property/${item.id}`)">
+        <v-container
+          fluid
+          grid-list-md>
+          <v-layout row wrap>
+            <v-flex
+              v-for="card in cards"
+              :key="card.title"
+              v-bind="{ [`xs${card.flex}`]: true }">
+              <v-card>
+                <v-img
+                  :src="card.src"
+                  height="200px">
+                  <v-container
+                    fill-height
+                    fluid
+                    pa-2>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text" v-text="card.title"></span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-img>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn icon>
+                    <v-icon>favorite</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>bookmark</v-icon>
+                  </v-btn>
+                  <v-btn icon>
+                    <v-icon>share</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 
