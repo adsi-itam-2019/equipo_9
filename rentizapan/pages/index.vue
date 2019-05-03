3f79591dd7d28cf919d78f791ae8f0a09f5a2f4d
diegovillafuerte <diegovillafuertesoraiz@gmail.com>
2019-05-03 07:30:24 -0500

    nuevas casas

--- rentizapan/pages/index.vue
+++ rentizapan/pages/index.vue
@@ -31,24 +31,21 @@
   export default {
     data: () => ({
       cards: [
-        { title: 'Casa no.98', address: 'Guerrero no. 12', price:'$1,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'1' },
-        { title: 'Casa no.2', address: 'Guerrero no. 24', price:'$2,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'2' },
-        { title: 'Casa no.43', address: 'Guerrero no. 321', price:'$3,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'3' },
-        { title: 'Casa no.19', address: 'Guerrero no. 465', price:'$4,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'4' },
-        { title: 'Casa no.11', address: 'Guerrero no. 532', price:'$5,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'5' },
-        { title: 'Casa no.13', address: 'Guerrero no. 632', price:'$6,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'6' },
-        { title: 'Casa no.2', address: 'Guerrero no. 62', price:'$2,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'7' },
-        { title: 'Casa no.6', address: 'Guerrero no. 24', price:'$8,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'8' },
-        { title: 'Casa no.5', address: 'Guerrero no. 4', price:'$9,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'9' },
-        { title: 'Casa no.32', address: 'Guerrero no. 243', price:'$1,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'10' },
-        { title: 'Casa no.1', address: 'Guerrero no. 98', price:'$2,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'11' },
-        { title: 'Casa no.21', address: 'Guerrero no. 65', price:'$4,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'12' },
-        { title: 'Casa no.56', address: 'Guerrero no. 432', price:'$5,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'13' },
-        { title: 'Casa no.78', address: 'Guerrero no. 643', price:'$2,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'14' },
-        { title: 'Casa no.9', address: 'Guerrero no. 987', price:'$6,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'15' },
-        { title: 'Casa no.35', address: 'Guerrero no. 912', price:'$2,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'16' },
-        { title: 'Casa no.90', address: 'Guerrero no. 513', price:'$8,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'17' },
-        { title: 'Casa no.75', address: 'Guerrero no. 892', price:'$2,000', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', id:'18' }
+        { title: 'Cuarto para estudiante cerca del ITAM', address: 'Guerrero no. 24', price:'$2,000', src: 'assets/img/c1.jpg', id:'1' },
+        { title: 'Bonita casa cerca de san angel', address: 'Guerrero no. 32', price:'$3,000', src: 'assets/img/c2.jpg', id:'2' },
+        { title: 'Casa de estudiantes cercana al ITAM', address: 'Guerrero no. 4', price:'$6,000', src: 'assets/img/c3.jpg', id:'3' },
+        { title: 'Casa en la calle de guerrero', address: 'Guerrero no. 16', price:'$7,500', src: 'assets/img/c4.jpg', id:'4' },
+        { title: 'Habitación estudiantil cerca del ITAM y la UNAM', address: 'Chihuahua no. 7', price:'$10,000', src: 'assets/img/c5.jpg', id:'5' },
+        { title: 'Vive cerca del itam en la casa más comoda', address: 'Chihuahua no. 11', price:'$8,000', src: 'assets/img/c6.jpg', id:'6' },
+        { title: 'Hermosa casa en colonia tizapan', address: 'Chihuahua no. 123', price:'$5,600', src: 'assets/img/c7.jpg', id:'7' },
+        { title: 'Casa en la colonia progreso', address: 'Chihuahua no. 782', price:'$4,900', src: 'assets/img/c8.jpg', id:'8' },
+        { title: 'Comoda casa a dos cuadras de periferico', address: 'Morelos no. 872', price:'$9,800', src: 'assets/img/c9.jpg', id:'9' },
+        { title: 'Casa compartida para estudiantes', address: 'Morelos no. 5', price:'$7,400', src: 'assets/img/c10.jpg', id:'10' },
+        { title: 'Cómodo cuarto para estudiante del ITAM (solo mujeres)', address: 'Morelos no. 8723', price:'$4,700', src: 'assets/img/c11.jpg', id:'11' },
+        { title: 'Cuarto cercano al ITAM (solo estudiantes)', address: 'Frontera no. 2', price:'$2,000', src: 'assets/img/c12.jpg', id:'12' },
+        { title: 'Casa en la calle frontera cercana al ITAM', address: 'Frontera no. 1', price:'$12,000', src: 'assets/img/c13.jpg', id:'13' },
+        { title: 'Amplia casa de estudiantes en colonia san angel', address: 'Frontera no. 56', price:'$19,000', src: 'assets/img/c14.jpg', id:'14' },
+        { title: 'Vive cerca del ITAM en la mejor casa', address: 'Frontera no. 92', price:'$8,650', src: 'assets/img/c15.jpg', id:'15' }
       ]
     })
   }
