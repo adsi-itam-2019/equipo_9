507f63f221bd8b98fea8987df3821b69018ab7f3
Jaime Limon Samperio <jaimelimonsamperio@MacBook-Pro-de-Jaime.local>
2019-05-13 18:50:27 -0500

    [UPDATE] :value en descripcion

--- rentizapan/pages/property/_id/index.vue
+++ rentizapan/pages/property/_id/index.vue
@@ -10,7 +10,7 @@
     <v-layout row wrap>
       <v-flex xs12 md5>
         <v-img
-        src="https://cdn.vuetifyjs.com/images/cards/house.jpg">
+        :src="data.imgUrl">
         </v-img>
         <!-- <v-container grid-list-xs>
           <v-layout row wrap>
@@ -30,21 +30,18 @@
 
         <h4 class="title mb-1">
           <v-icon class="mr-3">house</v-icon>
-          Departamento entero
+          Descripción
         </h4>
 
         <v-layout row wrap pl-5>
           <v-flex xs6 sm3 pt-0>
-            <p class="subheading">2 huéspedes</p>
+            <p class="subheading">{{ data.descripcion[0] }} dormitorios</p>
           </v-flex>
           <v-flex xs6 sm3 pt-0>
-            <p class="subheading">1 dormitorio</p>
+            <p class="subheading">{{ data.descripcion[1] }} camas</p>
           </v-flex>
           <v-flex xs6 sm3 pt-0>
-            <p class="subheading">2 camas</p>
-          </v-flex>
-          <v-flex xs6 sm3 pt-0>
-            <p class="subheading">1 baño</p>
+            <p class="subheading">{{ data.descripcion[2] }} baños</p>
           </v-flex>
         </v-layout>
 
@@ -71,28 +68,10 @@
         </h4>
 
         <v-layout row wrap mb-2>
-          <v-flex xs6>
+          <v-flex xs6 v-for="elem in data.servicios" :key="elem.label">
             <p class="subheading">
-              <v-icon>kitchen</v-icon>
-              Cocina
-            </p>
-          </v-flex>
-          <v-flex xs6>
-            <p class="subheading">
-              <v-icon>drive_eta</v-icon>
-              Estacionamiento
-            </p>
-          </v-flex>
-          <v-flex xs6 pt-0>
-            <p class="subheading">
-              <v-icon>ondemand_video</v-icon>
-              SmartTV
-            </p>
-          </v-flex>
-          <v-flex xs6 pt-0>
-            <p class="subheading">
-              <v-icon>wifi</v-icon>
-              WIFI
+              <v-icon>{{ elem.icon }}</v-icon>
+              {{ elem.label }}
             </p>
           </v-flex>
         </v-layout>
